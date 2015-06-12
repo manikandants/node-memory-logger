0a04564696d9bba38a07c99ef1ba4db1db1d7331
manikandanxenon <manikandan@wearexenon.com>
2015-06-12 22:52:20 +0530

    Added jasmine-node and coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function(grunt) {
 	'use strict';
 	var src = {
-		js : ['Gruntfile.js', 'index.js']
+		js : ['Gruntfile.js', 'index.js'],
+		spec : ['test/']
 	};
 	grunt.initConfig({
 		jshint : {
@@ -9,11 +10,44 @@ module.exports = function(grunt) {
 		},
 		jscs : {
 			all: src.js
+		},
+		/*jscs : disable*/
+		jasmine_node : {
+			all: {
+				options: {
+					coverage: {
+						print: 'detail',
+						reportDir:'reports/coverage/'
+					},
+					isVerbose: true,
+					showColors: true,
+					forceExit: true,
+					match: '.',
+					matchAll: false,
+					specFolders: src.spec,
+					extensions: 'js',
+					specNameMatcher: 'spec',
+					captureExceptions: true,
+					junitreport: {
+						report: false,
+						savePath : 'reports/jasmine/',
+						useDotNotation: true,
+						consolidate: true
+					}
+				},
+				src: src.js
+			}
+		},
+		coveralls : {
+			all : 'reports/coverage/lcov.info'
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-jasmine-node');
+	grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+	grunt.loadNpmTasks('grunt-coveralls');
 
-	grunt.registerTask('test', ['jshint' , 'jscs']);
+	grunt.registerTask('test', ['jshint', 'jscs', 'jasmine_node']);
 };
--- Readme.md
+++ Readme.md
@@ -1,10 +1,12 @@
 Memory Logger
 
 [![NPM](https://nodei.co/npm/memory-logger.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/memory-logger/)
+
 [![Build Status](https://travis-ci.org/manikandants/node-memory-logger.svg?branch=master)](https://travis-ci.org/manikandants/node-memory-logger)
 [![Dependency Status](https://david-dm.org/manikandants/node-memory-logger.svg)](https://david-dm.org/manikandants/node-memory-logger)
 [![devDependency Status](https://david-dm.org/manikandants/node-memory-logger/dev-status.svg)](https://david-dm.org/manikandants/node-memory-logger#info=devDependencies)
 [![Codacy Badge](https://www.codacy.com/project/badge/21919348601946fa8536109fab9c79ab)](https://www.codacy.com/app/manikandan-citeee/node-memory-logger)
+[![Code Climate](https://codeclimate.com/github/manikandants/node-memory-logger/badges/gpa.svg)](https://codeclimate.com/github/manikandants/node-memory-logger)
 
 ## Installation
 
